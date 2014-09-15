6a5b82115ae0aa784fb3fe92c0d5da9f1b4c9c5c
Jacques-Daniel PILLON <jdpillon@lesalternatives.org>
2014-09-15 15:05:58 +0000

    Add component tasks

---
+++ ui/tasks.reel/tasks.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/tasks.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Tasks
+ * @extends Component
+ */
+exports.Tasks = Component.specialize(/** @lends Tasks# */ {
+    constructor: {
+        value: function Tasks() {
+            this.super();
+        }
+    }
+});
